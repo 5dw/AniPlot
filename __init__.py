b33d45ae1a4253bd0d4351bb6e665653e37b0a49
xia.zhiquan <x.zhiquan@gmail.com>
2017-12-13 15:30:59 +0800

    add more method for axis

--- __init__.py
+++ __init__.py
@@ -24,17 +24,27 @@ class AniPlot():
                     else:
                         self.axis_list.append(self.fig.add_subplot(x, y, index))
 
-    def plot(self):
+    def get_plot(self):
         return self.plt
 
-    def figure(self):
+    def get_figure(self):
         return self.fig
 
-    def axis(self, index):
-        return self.axis_list[index]
+    def get_axis(self, index):
+        try:
+            axis = self.axis_list[index]
+            return axis
+        except Exception as err:
+            print(err)
 
     def axises(self):
         return self.axis_list
+
+    def plot(self, axis=0, *args, **kwargs):
+        try:
+            self.axis_list[axis].plot(*args, **kwargs)
+        except Exception as err:
+            print(err)
 
     def scatter(self, axis=0, *args, **kwargs):
         try:
@@ -48,3 +58,89 @@ class AniPlot():
             self.axis_list[axis].plot_surface(*args, **kwargs)
         except Exception as err:
             print(err)
+
+    def plot_date(self, axis=0, *args, **kwargs):
+            try:
+                self.axis_list[axis].plot_date(*args, **kwargs)
+            except Exception as err:
+                print(err)
+    def step(self, axis=0, *args, **kwargs):
+            try:
+                self.axis_list[axis].step(*args, **kwargs)
+            except Exception as err:
+                print(err)
+    def loglog(self, axis=0, *args, **kwargs):
+            try:
+                self.axis_list[axis].loglog(*args, **kwargs)
+            except Exception as err:
+                print(err)
+    def semilogx(self, axis=0, *args, **kwargs):
+            try:
+                self.axis_list[axis].semilogx(*args, **kwargs)
+            except Exception as err:
+                print(err)
+    def semilogy(self, axis=0, *args, **kwargs):
+            try:
+                self.axis_list[axis].semilogy(*args, **kwargs)
+            except Exception as err:
+                print(err)
+    def fill_between(self, axis=0, *args, **kwargs):
+            try:
+                self.axis_list[axis].fill_between(*args, **kwargs)
+            except Exception as err:
+                print(err)
+    def fill_betweenx(self, axis=0, *args, **kwargs):
+            try:
+                self.axis_list[axis].fill_betweenx(*args, **kwargs)
+            except Exception as err:
+                print(err)
+    def bar(self, axis=0, *args, **kwargs):
+            try:
+                self.axis_list[axis].bar(*args, **kwargs)
+            except Exception as err:
+                print(err)
+    def barh(self, axis=0, *args, **kwargs):
+            try:
+                self.axis_list[axis].barh(*args, **kwargs)
+            except Exception as err:
+                print(err)
+    def stem(self, axis=0, *args, **kwargs):
+            try:
+                self.axis_list[axis].stem(*args, **kwargs)
+            except Exception as err:
+                print(err)
+    def eventplot(self, axis=0, *args, **kwargs):
+            try:
+                self.axis_list[axis].eventplot(*args, **kwargs)
+            except Exception as err:
+                print(err)
+    def pie(self, axis=0, *args, **kwargs):
+            try:
+                self.axis_list[axis].pie(*args, **kwargs)
+            except Exception as err:
+                print(err)
+    def stackplot(self, axis=0, *args, **kwargs):
+            try:
+                self.axis_list[axis].stackplot(*args, **kwargs)
+            except Exception as err:
+                print(err)
+    def broken_barh(self, axis=0, *args, **kwargs):
+            try:
+                self.axis_list[axis].broken_barh(*args, **kwargs)
+            except Exception as err:
+                print(err)
+    def vlines(self, axis=0, *args, **kwargs):
+            try:
+                self.axis_list[axis].vlines(*args, **kwargs)
+            except Exception as err:
+                print(err)
+    def hlines(self, axis=0, *args, **kwargs):
+            try:
+                self.axis_list[axis].hlines(*args, **kwargs)
+            except Exception as err:
+                print(err)
+    def fill(self, axis=0, *args, **kwargs):
+            try:
+                self.axis_list[axis].fill(*args, **kwargs)
+            except Exception as err:
+                print(err)
